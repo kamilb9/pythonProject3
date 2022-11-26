17899be89382216346b36c19243aba4c06d8edf2
Kamil <kamilb9@gmail.com>
2022-11-26 12:22:45 +0100

    wypisanie stacji i temp

---
+++ imgw_request/main.py
@@ -0,0 +1,11 @@
+
+
+from requests import get
+
+url = 'https://danepubliczne.imgw.pl/api/data/synop/'
+
+response = get(url)
+pogoda_stacje=response.json()
+
+for stacja in pogoda_stacje:
+    print(stacja['stacja'], stacja['temperatura'], sep=' \t')
