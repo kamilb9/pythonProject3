6656da48e56ed23c533db33fef42bd38b6043e2b
Kamil <kamilb9@gmail.com>
2022-11-26 15:10:43 +0100

    commit3

--- imgw_request/zmniejszanie_obrazkow.py
+++ imgw_request/zmniejszanie_obrazkow.py
@@ -1,7 +1,17 @@
 from os import walk, path
+from PIL import Image
 
 print(list(walk('images')))
 
 for dir, _, files in walk('images'):
     for file in files:
-        print(path.join(dir, file))
+        # sciezka do obrazka
+       source = path.join(dir, file)
+        # otwieranie obrazka
+       image=Image.open(source)
+        # skalowanie obrazka
+       image.thumbnail((1080, 1080))
+        # tworzenie nowej nazwy dla zmniejszonych
+       zmniejszone=path.join(dir, 'zmniejszony_'+file)
+        # zapis zmniejszonego obrazka
+       image.save(zmniejszone)
