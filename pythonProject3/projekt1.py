c32f9e77ee9e328cab90acd63008a684e6e41635
Kamil <kamilb9@gmail.com>
2023-03-02 20:09:11 +0100

    first step 1

---
+++ pythonProject3/projekt1.py
@@ -0,0 +1,7 @@
+first_name = "Kamil"
+age = 36
+height = 183.5
+is_married = True
+
+print(f'Mam na imię {first_name}. Mam {age} lat.')
+print(f'Mam {height:.1f} cm wzrostu. Jestem {"żonaty" if is_married else "singlem"}.')
